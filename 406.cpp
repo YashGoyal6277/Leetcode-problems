6e2a4ae764828ce1866410a2df2edbcf2cbab0fa
YashGoyal6277 <71651231+YashGoyal6277@users.noreply.github.com>
2022-06-29 16:17:57 +0530

    Add files via upload

---
+++ 406.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+         vector<vector<int>> ans;
+         sort(people.begin(),people.end(),cmp);
+        for(int i=0;i<people.size();i++){
+            ans.insert(ans.begin()+people[i][1],people[i]);
+        }
+        return ans;
+    }
+     static bool cmp(vector<int> &a,vector<int> &b){
+        if(a[0]!=b[0]) return a[0]>b[0];
+         return a[1]<b[1];
+    }
+};
